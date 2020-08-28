176d59ca51e3ce587fd16f3a373a3f7fc869d014
Cesar Sportore <csportore@192.168.15.80>
2020-08-27 23:01:23 -0300

    Solution Angular style

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -2,15 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styles: [
+    `
+      .white-letters {
+        color: white;
+      }
+    `
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   isDisplayed:boolean = false;
   clickList = [];
+  defaultBGColor = 'white';
+  clickID = 0;
 
   toggleDisplay() {
     this.isDisplayed = !this.isDisplayed;
-    this.clickList.push(new Date());
+    this.clickList.push(++this.clickID);
+  }
+
+  paintBackground() {
+    return this.clickList.length >= 5 ? 'blue' : 'white';
   }
 }
